0fa8ef7b6e1428c97da6e4913da83e2b0a6b6e9b
Wilson Kiggundu <wilson@laboremus.no>
2017-09-20 08:38:08 +0300

    Initial commit

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,6 @@ from .views import *
 urlpatterns = [
     url(r'login/$', login, name='login'),
     url(r'user/details/$', details, name='user-details'),
+    url(r'user/register/$', register, name='register'),
+    url(r'user/password/reset/$', recover_password, name='recover_password'),
 ]
--- users/views.py
+++ users/views.py
@@ -13,3 +13,15 @@ def details(request):
     return render(request, "users/details.html", {
         'title': 'Login'
     })
+
+
+def register(request):
+    return render(request, "users/signup.html", {
+        'title': 'Register'
+    })
+
+
+def recover_password(request):
+    return render(request, "users/recover-password.html", {
+        'title': 'Reset Password'
+    })
